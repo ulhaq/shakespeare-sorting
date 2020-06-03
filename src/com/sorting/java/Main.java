8f174bbb16b3d860ca3a3fb7f55d0906a79540e7
unknown <ullhaq@hotmail.com>
2020-06-03 19:57:58 +0200

    rename

--- src/com/sorting/java/Main.java
+++ src/com/sorting/java/Main.java
@@ -8,14 +8,14 @@ import static com.sorting.java.FileReader.read;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         // Time complexity O(n^2)
-        List<String> SelList = read("src/com/sorting/resources/Shakespeare_Short.txt");
-        Selection.sorting(SelList);
-        System.out.println("Selection Sort: " + SelList.toString());
+//        List<String> SelList = read("src/com/sorting/resources/Shakespeare_Short.txt");
+//        Selection.sorting(SelList);
+//        System.out.println("Selection Sort: " + SelList.toString());
 //
 //        // Time complexity O(n^2)
-//        List<String> InsList = read("src/com/sorting/resources/Shakespeare_Short.txt");
-//        Insertion.sorting(InsList);
-//        System.out.println("Insertion Sort: " + InsList.toString());
+        List<String> InsList = read("src/com/sorting/resources/Shakespeare_Short.txt");
+        Insertion.sorting(InsList);
+        System.out.println("Insertion Sort: " + InsList.toString());
 
         // Time complexity O(n*log(n))
 //        List<String> HeaList = read("src/com/sorting/resources/Shakespeare_Short.txt");
--- src/com/sorting/java/Merge.java
+++ src/com/sorting/java/Merge.java
@@ -16,6 +16,7 @@ public class Merge implements Sort {
         if (from == to) {
             return;
         }
+
         int mid = (from + to) / 2;
         mergeSort(arr, from, mid);
         mergeSort(arr, mid + 1, to);
@@ -25,30 +26,30 @@ public class Merge implements Sort {
     public static void merge(List<String> arr, int from, int mid, int to) {
         int n = to - from + 1;
         String[] tempArray = new String[n];
-        int nextFirst = from;
-        int nextSecond = mid + 1;
+        int leftCursor = from;
+        int rightCursor = mid + 1;
         int j = 0;
 
-        while (nextFirst <= mid && nextSecond <= to) {
-            if (arr.get(nextFirst).compareTo(arr.get(nextSecond)) < 0) {
-                tempArray[j] = arr.get(nextFirst);
-                nextFirst++;
+        while (leftCursor <= mid && rightCursor <= to) {
+            if (arr.get(leftCursor).compareTo(arr.get(rightCursor)) < 0) {
+                tempArray[j] = arr.get(leftCursor);
+                leftCursor++;
             } else {
-                tempArray[j] = arr.get(nextSecond);
-                nextSecond++;
+                tempArray[j] = arr.get(rightCursor);
+                rightCursor++;
             }
             j++;
         }
 
-        while (nextFirst <= mid) {
-            tempArray[j] = arr.get(nextFirst);
-            nextFirst++;
+        while (leftCursor <= mid) {
+            tempArray[j] = arr.get(leftCursor);
+            leftCursor++;
             j++;
         }
 
-        while (nextSecond <= to) {
-            tempArray[j] = arr.get(nextSecond);
-            nextSecond++;
+        while (rightCursor <= to) {
+            tempArray[j] = arr.get(rightCursor);
+            rightCursor++;
             j++;
         }
 
